7639b169a3bdba36f65c81ed29845841c197f446
Masaru Ogawa <mogawa@c1r10s1.42tokyo.jp>
2023-04-24 18:19:31 +0900

    mandatory part completed with error handling

---
+++ _pipex_bonus.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   _pipex_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/12 17:24:05 by mogawa            #+#    #+#             */
+/*   Updated: 2023/04/24 15:25:13 by mogawa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "pipex.h"
+
+char	*ft_get_path(char *cmd)
+{
+	int		res;
+	char	*cmdpath;
+
+	cmdpath = ft_strjoin("/bin/", cmd);
+	res = access(cmdpath, F_OK);
+	if (res == 0)
+		return (cmdpath);
+	else
+		return (ft_strjoin("/usr/bin/", cmd));
+}
+
+void	ft_input(char *cmd)
+{
+	int	fd_input;
+
+	fd_input = open(cmd, O_RDONLY);
+	// todo open error - return 0?
+	dup2(fd_input, STDIN_FILENO);
+	close(fd_input);
+}
+
+void	ft_loop_argv(char *cmd, int	*prev_pipe, int pfd[])
+{
+	char		**argv;
+	extern char	**environ;
+	pid_t		pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (*prev_pipe != STDIN_FILENO)
+		{
+			dup2(*prev_pipe, STDIN_FILENO);
+			close(*prev_pipe);
+		}
+		dup2(pfd[WRITE], STDOUT_FILENO);
+		close(pfd[WRITE]);
+		argv = ft_split(cmd, ' ');
+		execve(ft_get_path(argv[0]), argv, environ);
+		perror("execve failed");
+		exit(1);	
+	}
+	else if (pid > 0)
+	{
+		close(*prev_pipe);
+		close(pfd[WRITE]);
+		*prev_pipe = pfd[READ];
+		wait(NULL);
+	}
+	else
+	{
+	// todo fork error
+	}
+}
+
+void	ft_output(int *prev_pipe, char *cmd, char *outfile)
+{
+	int			fd_output;
+	char		**argv;
+	extern char	**environ;
+
+	if (*prev_pipe != STDIN_FILENO)
+	{
+		dup2(*prev_pipe, STDIN_FILENO);
+		close(*prev_pipe);
+	}
+	fd_output = open(outfile, O_WRONLY | O_CREAT, 0777);
+	// todo open error
+	dup2(fd_output, STDOUT_FILENO);
+	close(fd_output);
+	if (fork() == 0)//! fork error
+	{
+		argv = ft_split(cmd, ' ');
+		execve(ft_get_path(argv[0]), argv, environ);
+		perror("last execve fail");
+		exit(1);
+	}
+	wait(NULL);
+}
+
+int	main(int argc, char **argv)
+{
+	int		n;
+	int		pfd[2];
+	int		prev_pipe;
+
+	if (argc < 5)
+	{
+		// todo error
+	}
+	else
+	{
+		ft_input(argv[1]);
+		n = 2;
+		prev_pipe = STDIN_FILENO;
+		while (n < argc - 2)
+		{
+			pipe(pfd);
+			ft_loop_argv(argv[n], &prev_pipe, pfd);
+			n++;
+		}
+		ft_output(&prev_pipe, argv[argc - 2], argv[argc - 1]);
+	}
+	return (0);
+}
--- pipex.c
+++ pipex.c
@@ -6,14 +6,14 @@
 /*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 17:24:05 by mogawa            #+#    #+#             */
-/*   Updated: 2023/04/24 14:27:58 by mogawa           ###   ########.fr       */
+/*   Updated: 2023/04/24 18:18:27 by mogawa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "pipex.h"
 
-char	*ft_get_path(char *cmd)
+static char	*ft_get_path(char *cmd)
 {
 	int		res;
 	char	*cmdpath;
@@ -26,97 +26,80 @@ char	*ft_get_path(char *cmd)
 		return (ft_strjoin("/usr/bin/", cmd));
 }
 
-void	ft_input(char *cmd)
+static void	ft_error(char *msg, bool do_exit)
 {
-	int	fd_input;
-
-	fd_input = open(cmd, O_RDONLY);
-	// todo open error - return 0?
-	dup2(fd_input, STDIN_FILENO);
-	close(fd_input);
+	perror(msg);
+	if (do_exit == true)
+		exit(-1);
 }
 
-void	ft_loop_argv(char *cmd, int	*prev_pipe, int pfd[])
+static void	ft_input(char *infile, char *cmd, int pfd[])
 {
-	char		**argv;
-	extern char	**environ;
-	pid_t		pid;
-
-	pid = fork();
-	if (pid == 0)
-	{
-		if (*prev_pipe != STDIN_FILENO)
-		{
-			dup2(*prev_pipe, STDIN_FILENO);
-			close(*prev_pipe);
-		}
-		dup2(pfd[WRITE], STDOUT_FILENO);
-		close(pfd[WRITE]);
-		argv = ft_split(cmd, ' ');
-		execve(ft_get_path(argv[0]), argv, environ);
-		perror("execve failed");
-		exit(1);	
-	}
-	else if (pid > 0)
-	{
-		close(*prev_pipe);
-		close(pfd[WRITE]);
-		*prev_pipe = pfd[READ];
-		wait(NULL);
-	}
-	else
-	{
-	// todo fork error
-	}
-}
-
-void	ft_output(int *prev_pipe, char *cmd, char *outfile)
-{
-	int			fd_output;
+	int			fd_infile;
 	char		**argv;
 	extern char	**environ;
 
-	if (*prev_pipe != STDIN_FILENO)
-	{
-		dup2(*prev_pipe, STDIN_FILENO);
-		close(*prev_pipe);
-	}
-	fd_output = open(outfile, O_WRONLY | O_CREAT, 0777);
-	// todo open error
-	dup2(fd_output, STDOUT_FILENO);
-	close(fd_output);
-	if (fork() == 0)//! fork error
-	{
-		argv = ft_split(cmd, ' ');
-		execve(ft_get_path(argv[0]), argv, environ);
-		perror("last execve fail");
-		exit(1);
-	}
-	wait(NULL);
+	close(pfd[READ]);
+	fd_infile = open(infile, O_RDONLY);
+	if (fd_infile == -1)
+		ft_error(infile, true);
+	if (dup2(fd_infile, STDIN_FILENO) == -1)
+		ft_error("pipex", true);
+	close(fd_infile);
+	if (dup2(pfd[WRITE], STDOUT_FILENO) == -1)
+		ft_error("pipex", true);
+	close(pfd[WRITE]);
+	argv = ft_split(cmd, ' ');
+	execve(ft_get_path(argv[0]), argv, environ);
+	ft_error(argv[0], true);
+}
+
+static void	ft_output(char *outfile, char *cmd, int pfd[])
+{
+	int			fd_outfile;
+	char		**argv;
+	extern char	**environ;
+
+	close(pfd[WRITE]);
+	fd_outfile = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd_outfile == -1)
+		ft_error(outfile, true);
+	if (dup2(pfd[READ], STDIN_FILENO) == -1)
+		ft_error(outfile, true);
+	close(pfd[READ]);
+	if (dup2(fd_outfile, STDOUT_FILENO) == -1)
+		ft_error(outfile, true);
+	close(fd_outfile);
+	argv = ft_split(cmd, ' ');
+	execve(ft_get_path(argv[0]), argv, environ);
+	ft_error(argv[0], true);
 }
 
 int	main(int argc, char **argv)
 {
-	int		n;
 	int		pfd[2];
-	int		prev_pipe;
+	pid_t	pid;
 
-	if (argc < 5)
+	if (argc == 5)
 	{
-		// todo error
-	}
-	else
-	{
-		ft_input(argv[1]);
-		n = 2;
-		prev_pipe = STDIN_FILENO;
-		while (n < argc - 2)
+		if (pipe(pfd) == 0)
 		{
-			pipe(pfd);
-			ft_loop_argv(argv[n], &prev_pipe, pfd);
-			n++;
+			pid = fork();
+			if (pid == 0)
+			{
+				ft_input(argv[1], argv[2], pfd);
+			}
+			else if (pid > 0)
+			{
+				ft_output(argv[4], argv[3], pfd);
+			}
+			else
+				ft_error("FORKING FAILED", false);
+			wait(NULL);
+			wait(NULL);
 		}
-		ft_output(&prev_pipe, argv[argc - 2], argv[argc - 1]);
+		else
+			ft_error("PIPING FAILED", false);
 	}
 	return (0);
 }
